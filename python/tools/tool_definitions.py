322538907a27db842e74b2fd414fc983c4ebde15
d-r-w <derek@dwhite.codes>
2025-03-09 16:20:49 -0500

    Remove unused tool

--- python/tools/tool_definitions.py
+++ python/tools/tool_definitions.py
@@ -1,23 +1,5 @@
 def get_tool_definitions():
   return [
-      {
-        "type": "function",
-        "function": {
-            "name": "get_current_weather",
-            "description": "Get the current weather",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "location": {
-                        "type": "string",
-                        "description": "The city and country, eg. San Francisco, USA"
-                    },
-                    "format": { "type": "string", "enum": ["celsius", "fahrenheit"] }
-                },
-                "required": ["location", "format"]
-            }
-        }
-    },
     {
         "type": "function",
         "function": {
