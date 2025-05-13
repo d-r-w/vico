7ecf0c26ca873d066d7d4731cdd02968bc172621
d-r-w <derek@dwhite.codes>
2025-05-13 08:46:31 -0500

    Use current date

--- data/wiki/update_wiki.sh
+++ data/wiki/update_wiki.sh
@@ -1,4 +1,4 @@
-D=20250501
+D=$(date +%Y%m01)
 BASE=https://dumps.wikimedia.org/enwiki/$D
 curl -L -O -C - "$BASE/enwiki-${D}-pages-articles-multistream.xml.bz2"
 curl -L -O -C - "$BASE/enwiki-${D}-pages-articles-multistream-index.txt.bz2"
